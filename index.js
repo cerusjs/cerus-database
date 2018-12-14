03af3a1bbf0109daf26d00e85f3527c24e259fec
JortvD <jortvandriel@hotmail.com>
2018-12-14 19:57:15 +0100

    Let databases be called directly instead of having to access the manager class

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ module.exports = function() {
 	plugin.name = package["name"];
 	plugin.version = package["version"];
 
-
 	plugin._init = function(cerus) {
 		database = new (require("./lib/database"))(cerus);
 	}
 
-	plugin.database = function() {
+	plugin.database = function(name) {
+		if(typeof name === "string") return database.database(name);
+
 		return database;
 	}
 
